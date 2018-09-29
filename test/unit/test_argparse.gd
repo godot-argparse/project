20a0c880a0c40fa922d1c4c6f7eea394caad1716
mashumafi <mashumafi@gmail.com>
2018-09-29 02:20:16 +0000

    fix paths

--- test/unit/test_argparse.gd
+++ test/unit/test_argparse.gd
@@ -1,6 +1,6 @@
 extends "res://addons/gut/test.gd"
 
 func test_new():
-	var script = load("res://addon/godot-argparser/argparser.gd")
+	var script = load("res://addons/godot-argparse/argparse.gd")
 	var instance = script.new()
 	var parser = instance.ArgumentParser.new()
