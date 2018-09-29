8010fe79b0f38b202fd4b313bc1da73e5798ba75
mashumafi <mashumafi@gmail.com>
2018-09-29 02:51:28 +0000

    test proj accessor

--- test/integration/test_argparser.gd
+++ test/integration/test_argparser.gd
@@ -1,5 +1,5 @@
 extends "res://addons/gut/test.gd"
 
 func test_addon():
-	var parser = argparse.ArgumentParser("test")
-	assert_eq("test", parser.proj)
+	var parser = argparse.ArgumentParser()
+	assert_get_set_methods(parser, "proj", null, "test")
